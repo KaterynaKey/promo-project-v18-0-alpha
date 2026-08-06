2ede126cfd5a364208c581d0c0bd0691a98094ed
KaterynaKey <kayakeytrip@gmail.com>
2026-08-06 16:31:40 +0200

    Update main.js
    fix

--- js/main.js
+++ js/main.js
@@ -649,11 +649,12 @@ async function exportHTML() {
     editorContent = wrapSignatureImg(editorContent);
     editorContent = wrapFooterBlock(editorContent);
     editorContent = wrapFooterCenterBlock(editorContent);
-    editorContent = cleanEmptyHtmlTags(editorContent);
-    editorContent = wrapContentInFullTableStructure(editorContent);
+  
 
     editorContent = addOneBr(editorContent);
     editorContent = replaceTripleBrWithSingle(editorContent);
+    editorContent = cleanEmptyHtmlTags(editorContent);
+    editorContent = wrapContentInFullTableStructure(editorContent);
 
     const prettyHtml = await formatWithPrettier(editorContent);
     document.getElementById('output').value = prettyHtml;
